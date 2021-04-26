1cfc928a0bb503b1edb0915aec85416bf2059053
Michael Thorsager <thorsager@gmail.com>
2021-04-26 16:28:00 +0200

    Fixing init-script, so that it actually works

--- from-env.sh
+++ from-env.sh
@@ -2,13 +2,15 @@
 if [ ! -z "$MYSQL_DATABASES" ]; then 
 	readarray -td, a <<< "$MYSQL_DATABASES"
 
+	mysql_note "********* quck-init ***********"
 	for db in "${a[@]}"; do 
 		db_name=${db//[$'\n' ]/}
-		echo "Create and grant for $db_name"
-		"${mysql[@]}" <<-EOSQL
-				CREATE DATABASE $db_name;
-				GRANT ALL ON $db_name.* TO '$MYSQL_USER'@'${MYSQL_HOST:-%}';
+		mysql_note "Create and grant for $MYSQL_USER on $db_name"
+		docker_process_sql <<-EOSQL
+		CREATE DATABASE $db_name; 
+		GRANT ALL ON $db_name.* TO '$MYSQL_USER'@'${MYSQL_HOST:-%}';
 		EOSQL
 	done
+	mysql_note "*******************************"
 fi
 
